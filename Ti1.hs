b86c012989ec0dd1b7727eb278de9e44dce42d34
khubaibkay37 <54719412+khubaibkay37@users.noreply.github.com>
2025-05-19 08:36:25 -0500

    Fix Ti1

--- Ti1.hs
+++ Ti1.hs
@@ -1,45 +1,43 @@
 module Ti1 where
 
-import CoreLanguage
-import Utils
+import CoreLanguage                -- CoreProgram, CoreExpr, CoreScDefn, preludeDefs
+import Utils                        -- Addr, Heap, Assoc, hInitial, hAlloc, hLookup, mapAccuml, aLookup
+import Control.Monad                -- for forM_
 
 -- Machine types
+-- Stack of node addresses, heap and globals
 type TiStack   = [Addr]
 type TiDump    = [TiStack]
 type TiHeap    = Heap Node
 type TiGlobals = [(Name, Addr)]
 type TiState   = (TiStack, TiDump, TiHeap, TiGlobals, [CoreScDefn])
 
--- Heap nodes (Mark 1 only needs NAp, NSupercomb, NNum)
+-- Heap nodes for Mark 1
 data Node
-  = NAp Addr Addr
-  | NSupercomb Name [Name]
-  | NNum Int
-  deriving Show
+  = NAp Addr Addr             -- application node
+  | NSupercomb Name [Name]    -- supercombinator template
+  | NNum Int                  -- integer literal
+  deriving (Show)
 
--- | Build the initial heap and globals from a list of supercombinator defs
+-- Build initial heap and global address map, including prelude + program
 buildInitialHeap :: [CoreScDefn] -> (TiHeap, TiGlobals)
-buildInitialHeap defs
-  = (heap', globals)
+buildInitialHeap defs = (heap', globals)
   where
-    names          = [name | (name, _, _) <- defs]
+    names          = [name | (name,_,_) <- defs]
     (heap', addrs) = mapAccuml allocateSc hInitial defs
     globals        = zip names addrs
 
 allocateSc :: TiHeap -> CoreScDefn -> (TiHeap, Addr)
-allocateSc heap (name, args, _)
-  = hAlloc heap (NSupercomb name args)
+allocateSc heap (name, args, _) = hAlloc heap (NSupercomb name args)
 
--- | Initial machine state: stack points at “main”
+-- Initialise machine state: stack at 'main'
 initialTiState :: CoreProgram -> TiState
-initialTiState program
-  = ([addrMain], [], heap0, globals, program)
+initialTiState program = ([addrMain], [], heap0, globals, program)
   where
     (heap0, globals) = buildInitialHeap (preludeDefs ++ program)
-    addrMain         = aLookup globals "main"
-                         (error "main is not defined")
+    addrMain         = aLookup globals "main" (error "main is not defined")
 
--- | The driver: produce the sequence of states from initial to final
+-- Evaluate from initial to final state sequence
 eval :: CoreProgram -> [TiState]
 eval program = state0 : rest state0
   where
@@ -48,89 +46,93 @@ eval program = state0 : rest state0
       | tiFinal st = []
       | otherwise  = let st' = step st in st' : rest st'
 
--- | Final when stack points at a NNum and dump is empty
+-- Final when stack points at NNum
 tiFinal :: TiState -> Bool
-tiFinal ([addr], [], heap, _, _)
-  = case hLookup heap addr of
-      NNum _ -> True
-      _      -> False
+tiFinal ([addr], [], heap, _, _) =
+  case hLookup heap addr of
+    NNum _ -> True
+    _      -> False
 tiFinal _ = False
 
--- | One transition
+-- Single transition
 step :: TiState -> TiState
 step state@(stack, dump, heap, globals, progDefs) =
   dispatch (hLookup heap (head stack)) state
 
--- | Apply the two Mark 1 rules: Unwind and Supercomb
+-- Dispatch the two Mark 1 rules
 dispatch :: Node -> TiState -> TiState
 
--- Unwind: if top of stack is NAp, push the function address
-dispatch (NAp a1 _) (stack, dump, heap, globals, progDefs)
-  = (a1 : stack, dump, heap, globals, progDefs)
+-- Unwind: application node, push function part
+dispatch (NAp a1 _) (stack, dump, heap, globals, progDefs) =
+  (a1 : stack, dump, heap, globals, progDefs)
 
--- Supercombinator step
+-- Supercombinator: instantiate its body
 dispatch (NSupercomb sc args) (stack, dump, heap, globals, progDefs)
-  | length stack - 1 < length args
-    = error $ "Too few args for supercombinator " ++ sc
-  | otherwise
-    = (newStack, dump, heap', globals, progDefs)
-  where
-    -- Extract the argument addresses from the stack
-    argAddrs          = take (length args) (tail stack)
-    env               = zip args argAddrs
+  | length stack - 1 < length args = error $ "Too few args for supercombinator " ++ sc
+  | otherwise =
+      let apAddrs  = take (length args) (tail stack)
+          argAddrs = [ case hLookup heap ap of
+                         NAp _ a2 -> a2
+                         _        -> error "Malformed application node"
+                     | ap <- apAddrs ]
+          env      = zip args argAddrs
+          body     = findScBody sc (preludeDefs ++ progDefs)
+          (heap', rootAddr) = instantiate body heap globals env
+          newStack = rootAddr : drop (length args + 1) stack
+      in  (newStack, dump, heap', globals, progDefs)
 
-    -- Lookup the body in our global program defs (prelude ++ user)
-    body              = findScBody sc (preludeDefs ++ progDefs)
+dispatch _ _ = error "Cannot dispatch this node in Mark 1"
 
-    -- Instantiate with access to both locals *and* globals
-    (heap', rootAddr) = instantiate body heap globals env
-
-    -- New stack: root of instantiated graph replaces old application
-    newStack          = rootAddr : drop (length args + 1) stack
-
--- | Instantiate a CoreExpr into the heap, returning (heap', addr)
-instantiate
-  :: CoreExpr
-  -> TiHeap
-  -> TiGlobals
-  -> [(Name, Addr)]
-  -> (TiHeap, Addr)
-
-instantiate (ENum n) heap _ _ =
-  hAlloc heap (NNum n)
-
--- FIRST try the local env; otherwise fall back to globals
-instantiate (EVar v) heap globals env
+-- Instantiate CoreExpr into heap, with locals and globals
+type Env = [(Name, Addr)]
+instantiate :: CoreExpr -> TiHeap -> TiGlobals -> Env -> (TiHeap, Addr)
+instantiate (ENum n)   heap _       _   = hAlloc heap (NNum n)
+instantiate (EVar v)   heap globals env
   = case lookup v env of
       Just addr -> (heap, addr)
-      Nothing   -> (heap, aLookup globals v
-                              (error $ "Unbound global: " ++ v))
-
+      Nothing   -> (heap, aLookup globals v (error $ "Unbound global: " ++ v))
 instantiate (EAp e1 e2) heap globals env
-  = let (heap1, a1) = instantiate e1 heap    globals env
-        (heap2, a2) = instantiate e2 heap1   globals env
-    in  hAlloc heap2 (NAp a1 a2)
+  = let (h1, a1) = instantiate e1 heap    globals env
+        (h2, a2) = instantiate e2 h1       globals env
+    in  hAlloc h2 (NAp a1 a2)
+instantiate _ _ _ _ = error "Cannot instantiate this expression in Mark 1"
 
--- | Find the body of a supercombinator from the defns
+-- Lookup supercombinator body
 findScBody :: Name -> [CoreScDefn] -> CoreExpr
 findScBody name defs =
-  case [ body | (n, _, body) <- defs, n == name ] of
+  case [body | (n, _, body) <- defs, n == name] of
     b : _ -> b
     []    -> error $ "Supercombinator not found: " ++ name
 
--- | Pretty-print a state for testing
+-- Debug printing
 showState :: TiState -> String
-showState (stack, _, heap, globals, _)
-  =  "Stack: "  ++ show stack  ++ "\n"
-  ++ "Heap:\n" ++ show heap
-  ++ "Globals: " ++ show globals ++ "\n"
+showState (stack, _, heap, globals, _) =
+  "Stack: " ++ show stack ++ "\n" ++
+  "Heap:\n" ++ showHeap heap ++
+  "Globals: " ++ show globals ++ "\n"
 
--- | A tiny example and tester
-exampleProg1 :: CoreProgram
-exampleProg1 =
-  [ ("b",    ["x"], (ENum 5))
-      , ("main", [],   EAp (EVar "b") (ENum 3))
+showHeap :: Heap Node -> String
+showHeap (_, as) = unlines [ show addr ++ ": " ++ show node | (addr, node) <- as ]
+
+-- | Run and print each state, then final result
+run :: CoreProgram -> IO ()
+run program = do
+  let states = eval program
+  forM_ (zip [0..] states) $ \(i, st) -> do
+    putStrLn $ "State " ++ show i
+    putStrLn $ showState st
+  let (stack, _, heap, _, _) = last states
+      [finalAddr] = stack
+      NNum result = hLookup heap finalAddr
+  putStrLn $ "Final result = " ++ show result
+
+-- A tiny example
+exampleProg0 :: CoreProgram
+exampleProg0 =
+  [ ("I",    ["x"], EVar "x")
+  , ("main", [],     EAp (EVar "I") (ENum 69))
   ]
 
+-- Default test
 test :: IO ()
-test = mapM_ (putStrLn . showState) (eval exampleProg1)
+test = run exampleProg0
